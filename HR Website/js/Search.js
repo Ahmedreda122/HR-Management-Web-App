4bdf7af31b9dd6a7926015f96249c3d702484bcb
Abdallah Hussein <abiza2013@gmail.com>
2023-04-14 17:01:53 +0200

    search, remove and some styling

--- HR Website/js/Search.js
+++ HR Website/js/Search.js
@@ -7,15 +7,15 @@ srchInput.addEventListener("input", function () {
   let found = false;
   const item = srchInput.value.toLowerCase();
   for (let i = 2; i < rows.length; i++) {
-    // start from 1 as we don't need to include the thead (table header)
+    // start from 2 as we don't need to include the thead (table header)
     const name = rows[i]
       .getElementsByTagName("td")[1]
       .textContent.toLocaleLowerCase();
-    //textContent only applies to elements that can contain text, such as <p> or <span>
+    // textContent only applies to elements that can contain text, such as <p> or <span>
     // value returns the current value entered by the user
     // loop through table on column name which is column 1 and get its textContent
     if (name.includes(item)) {
-      rows[i].style.display = "";
+      rows[i].style.display = ""; // default
       found = true;
     } else {
       rows[i].style.display = "none"; // hide other rows that don't match with the input
@@ -25,6 +25,27 @@ srchInput.addEventListener("input", function () {
     cption.style.display = "none";
     rows[1].style.display = "none"; // hide the table header
     rows[0].style.display = "block"; // display the word NOT FOUND!
-    rows[0].style.fontSize = "20px";
-}
+  } else {
+    // return caption and table header to its default styles
+    cption.style.display = "";
+    rows[1].style.display = "";
+    rows[0].style.display = "";
+  }
 });
+
+const rmvbtn = document.getElementsByClassName("remove"); // array of all remove buttons
+for (let i = 0; i < rmvbtn.length; i++) {
+  rmvbtn[i].onclick = function () {
+    if (confirm("Are you sure to remove this employee?")) {
+      rows[i + 2].remove();
+    }
+  };
+}
+// const rmvbtn = document.getElementsByClassName("remove"); // array of all remove buttons
+// for (let i = 0; i < rmvbtn.length; i++) {
+//   rmvbtn[i].onclick = function () {
+//     if (confirm("Are you sure to remove this employee?")) {
+//       rmvbtn[i].closest("tr").parentNode.removeChild(rmvbtn[i].closest("tr"));
+//     }
+//   };
+// }
