2446eaee6b7d90aaa3ebe2190cfc31b1fa8ef1c8
Abdallah Hussein <abiza2013@gmail.com>
2023-04-18 17:44:44 +0200

    Submit Vacation Validation with local storage

--- HR Website/js/SubmitVac.js
+++ HR Website/js/SubmitVac.js
@@ -1,8 +1,4 @@
-const validIds =
-	"20230000,20230001,20230002,20230003,20230004,20230005,20230006,20230007";
-localStorage.setItem("validIds", validIds);
-
-const hidden = document.getElementsByClassName("hidden-div");
+const employees = JSON.parse(window.localStorage.getItem("employees"));
 
 // Find the submit button
 const submitBtn = document.getElementById("submitBtn");
@@ -13,48 +9,50 @@ const endD = document.getElementById("endD");
 
 // Add a click event listener to the submit button
 document.forms[0].onsubmit = function (event) {
-	const valid = localStorage.getItem("validIds").split(",");
-	// Check if the input ID is in the list of valid IDs
-	let checkid = false;
-	if (valid.includes(idInput.value) && idInput !== "") {
-		// hidden[0].style.display = "block";
-		// event.preventDefault();
-		checkid = true;
-		// console.log("done")
-	} else {
-		event.preventDefault();
-		// ID is invalid, show an error message
-		idInput.focus();
-		idInput.style.borderColor = "red";
-		idInput.addEventListener("input", function () {
-			idInput.style.borderColor = ""; // set to default border color
-		});
-		alert("Invalid ID");
-	}
-	let checkDate = false;
-	const start = new Date(startD.value);
-	const end = new Date(endD.value);
-	if (start >= end) {
-		event.preventDefault();
-		alert("End date must be after start date");
-		endD.focus();
-		endD.style.borderColor = "red";
-		startD.style.borderColor = "red";
-		startD.addEventListener("input", function () {
-			startD.style.borderColor = ""; // set to default border color
-		});
-		endD.style.borderColor = "red";
-		endD.addEventListener("input", function () {
-			endD.style.borderColor = ""; // set to default border color
-		});
-	} else {
-		checkDate = true;
-	}
-	if (checkDate && checkid) {
-		event.preventDefault();
-		alert("Vacation Submitted");
-		window.location.href = "H Home.html";
-	}
-	// console.log("Checking date is : " + start)
-	// console.log("Checking date is : " + end)
+  // Check if the input ID is in the list of valid IDs
+  let checkid = false;
+  for (let i = 0; i < employees.length; i++) {
+    if (employees[i].ID.includes(idInput.value) && idInput !== "") {
+      // event.preventDefault();
+      checkid = true;
+      // console.log("done")
+    }
+  }
+  if (!checkid) {
+    event.preventDefault();
+    // ID is invalid, show an error message
+    idInput.focus();
+    idInput.style.borderColor = "red";
+    idInput.addEventListener("input", function () {
+      idInput.style.borderColor = ""; // set to default border color
+    });
+    alert("Invalid ID");
+  }
+
+  let checkDate = false;
+  const start = new Date(startD.value);
+  const end = new Date(endD.value);
+  if (start >= end) {
+    event.preventDefault();
+    alert("End date must be after start date");
+    endD.focus();
+    endD.style.borderColor = "red";
+    startD.style.borderColor = "red";
+    startD.addEventListener("input", function () {
+      startD.style.borderColor = ""; // set to default border color
+    });
+    endD.style.borderColor = "red";
+    endD.addEventListener("input", function () {
+      endD.style.borderColor = ""; // set to default border color
+    });
+  } else {
+    checkDate = true;
+  }
+  if (checkDate && checkid) {
+    event.preventDefault();
+    alert("Vacation Submitted");
+    window.location.href = "H Home.html";
+  }
+  // console.log("Checking date is : " + start)
+  // console.log("Checking date is : " + end)
 };
