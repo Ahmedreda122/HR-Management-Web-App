9b8bc104bb5e73983d79552dfc8ebd0399bd57c3
Abdallah Hussein <abiza2013@gmail.com>
2023-05-22 19:02:37 +0300

    Completing Transfer  the Manager files into Django

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -11,14 +11,29 @@ def index(request):
     # }
     return render(request, 'HRWebsite/Home.html',)
 
+
 def login(request):
     return render(request, 'HRWebsite/Log in.html')
 
 # def hey(request):
 #     template = "Emp/index.html"
 
+
 def HrHome(request):
     return render(request, 'HRWebsite/H Home.html')
 
+
 def MHome(request):
     return render(request, 'HRWebsite/M Home.html')
+
+
+def AddHR(request):
+    return render(request, 'HRWebsite/Add HR.html')
+
+
+def ShowHRs(request):
+    return render(request, 'HRWebsite/Show HR.html')
+
+
+def VacationAction(request):
+    return render(request, 'HRWebsite/Vacation Action.html')
