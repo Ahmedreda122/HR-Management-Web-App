4b949b06ea2cfc47c3a6bcd6b57f2f9dc6b6923d
Abdallah Hussein <abiza2013@gmail.com>
2023-05-25 01:18:33 +0300

    Merging

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, HR
 
 # Register your models here.
 admin.site.register(Employee)
+admin.site.register(HR)
 #Username: Hope
 #Email address: ahmadredaby122@gmail.com
 #Password: Hey123456
+
