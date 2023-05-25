c838b63cf00a09a5af9e0fdaaa52ab13303e5595
Abdallah Hussein <abiza2013@gmail.com>
2023-05-25 03:32:39 +0300

    Fixing a bug in Add Employee

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -7,22 +7,20 @@ from .models import Vacation
 
 
 class EmployeeForm(forms.ModelForm):
-    ID = forms.IntegerField(
-        widget=forms.NumberInput(attrs={'disabled': True}),
-        label='ID',
-        initial=Employee.objects.latest('ID').ID + 1
-    )
-    userName = forms.CharField(max_length=100)
-    Email = forms.CharField(max_length=255)
-    phoneNum = forms.IntegerField()
+    ID = forms.IntegerField(label='ID')
+    userName = forms.CharField(max_length=100, label='Name')
+    Email = forms.EmailField(max_length=255)
+    phoneNum = forms.IntegerField(label='Phone Number')
     address = forms.CharField(max_length=255)
-    birthDate = forms.DateField(widget=DateInput(attrs={'type': 'date'}))
+    birthDate = forms.DateField(widget=DateInput(
+        attrs={'type': 'date'}), label='Birth Date', initial='2000-01-01')
     gender = forms.ChoiceField(
         choices=[('male', 'Male'), ('female', 'Female')])
-    jobTitle = forms.CharField(max_length=64)
-    maritalStatus = forms.CharField(max_length=64)
+    jobTitle = forms.CharField(max_length=64, label='Job Title')
+    maritalStatus = forms.ChoiceField(label='Marital Status', choices=[(
+        'Single', 'Single'), ('Married', 'Married'), ('Divorced', 'Divorced')])
     salary = forms.IntegerField()
-    vacationDays = forms.IntegerField()
+    vacationDays = forms.IntegerField(label='Vacation Days')
 
     class Meta:
         model = Employee
@@ -42,9 +40,12 @@ class EmployeeForm(forms.ModelForm):
 
         fields = '__all__'
  # The constructor
+
     def __init__(self, *args, **kwargs):
         # Call the parent class __init__ method
         super().__init__(*args, **kwargs)
+        self.fields['ID'].disabled = True
+        self.fields['ID'].initial = Employee.objects.latest('ID').ID + 1
         self.order_fields([
             'ID',  # Display the 'ID' field First
             'userName',
